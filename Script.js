57247b30e564c64d356ccdc7682931c67a42a87e
cris-we <cofrecristofer098@gmail.com>
2026-03-31 15:25:42 -0300

    Enhance click event handling for card interaction

--- Script.js
+++ Script.js
@@ -1,8 +1,20 @@
 const envoltura = document.querySelector(".envoltura-sobre");
 const carta = document.querySelector(".carta");
-document.addEventListener("click", () =>){
 
+document.addEventListener("click", (e) => {
+    if (e.target.matches(".sobre") ||
+        e.target.matches(".solapa-derecha") ||
+        e.target.matches(".solapa-izquierda") ||
+        e.target.matches(".corazon")) {
 
+        envoltura.classList.toggle("abierto");
 
-  
-})
+        if (envoltura.classList.contains("abierto")) {
+            setTimeout(() => {
+                carta.classList.add("mostrar-carta");
+            }, 500);
+        } else {
+            carta.classList.remove("mostrar-carta");
+        }
+    }
+});
